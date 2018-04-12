5ab599341d7f53c3e3f2103d69e819a53fc83f80
Smithface <kylewibblesmith@gmail.com>
2018-04-12 19:00:59 -0400

    finished stretch goal
    need to digest what code is actually doing

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -15,7 +15,26 @@ const POP = 0b01001100;
 const CALL = 0b01001000;
 const RET = 0b00001001;
 const ADD = 0b10101000;
-const SP = 0x07;
+const ST = 0b10011010;
+const JMP = 0b01010000;
+const PRA = 0b01000010;
+const IRET = 0b00001011;
+
+const IM = 0x05;  // Interrupt mask register R5
+const IS = 0x06;  // Interrupt status register R6
+const SP = 0x07;  // Stack Pointer
+
+// Interrupt mask bits
+const intMask = [
+    (0x1 << 0), // timer
+    (0x1 << 1), // keyboard
+    (0x1 << 2), // reserved
+    (0x1 << 3), // reserved
+    (0x1 << 4), // reserved
+    (0x1 << 5), // reserved
+    (0x1 << 6), // reserved
+    (0x1 << 7), // reserved
+];
 
 class CPU {
 
@@ -30,22 +49,32 @@ class CPU {
         
         // Special-purpose registers
         this.reg.PC = 0; // Program Counter
-        this.reg[SP] = 0xf4;
+        this.reg.FL = 0; // Flags
+
+        this.interruptsEnabled = true;
+
+        this.reg[SP] = 0xf4;    // Stack empty
+        this.reg[IM] = 0;       // All interrupts masked
+        this.reg[IS] = 0;       // No interrupts active
         this.setupBranchTable();
     }
 
     setupBranchTable() {
         let bt = {}
 
-        bt[LDI] = this.handle_LDI;
-        bt[HLT] = this.handle_HLT;
-        bt[PRN] = this.handle_PRN;
-        bt[MUL] = this.handle_MUL;
+        bt[ST] = this.handle_ST;
         bt[ADD] = this.handle_ADD;
-        bt[PUSH] = this.handle_PUSH;
+        bt[HLT] = this.handle_HLT;
+        bt[JMP] = this.handle_JMP;
+        bt[LDI] = this.handle_LDI;
+        bt[MUL] = this.handle_MUL;
         bt[POP] = this.handle_POP;
-        bt[CALL] = this.handle_CALL;
+        bt[PRA] = this.handle_PRA;
+        bt[PRN] = this.handle_PRN;
         bt[RET] = this.handle_RET;
+        bt[CALL] = this.handle_CALL;
+        bt[IRET] = this.handle_IRET;
+        bt[PUSH] = this.handle_PUSH;
 
         this.branchTable = bt;
     }
@@ -58,12 +87,26 @@ class CPU {
     }
 
     /**
+   * Raise an interrupt
+   * 
+   * @param n Interrupt number, 0-7
+   */
+    raiseInterrupt(n) {
+        this.reg[IS] |= intMask[n];
+    }
+
+    /**
      * Starts the clock ticking on the CPU
      */
     startClock() {
         this.clock = setInterval(() => {
             this.tick();
         }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+
+        this.timerInterrupt = setInterval(() => {
+            // Set the timer bit in the IS register
+            this.raiseInterrupt(0);
+        }, 1000);
     }
 
     /**
@@ -71,6 +114,7 @@ class CPU {
      */
     stopClock() {
         clearInterval(this.clock);
+        clearInterval(this.timerInterrupt);
     }
 
     /**
@@ -99,6 +143,54 @@ class CPU {
      * Advances the CPU one cycle
      */
     tick() {
+
+        if (this.interruptsEnabled) {
+            // Take the current interrupts and mask them out with the interrupt
+            // mask
+            const maskedInterrupts = this.reg[IS] & this.reg[IM];
+      
+            // Check all the masked interrupts to see if they're active
+            for (let i = 0; i < 8; i++) {
+              
+              // If it's still 1 after being masked, handle it
+              if (((maskedInterrupts >> i) & 0x01) === 1) {
+      
+                // Only handle one interrupt at a time
+                this.interruptsEnabled = false;
+      
+                // Clear this interrupt in the status register
+                this.reg[IS] &= ~intMask[i];
+      
+                // Push return address
+                this.reg[SP] = this.reg[SP] - 1;
+                this.ram.write(this.reg[SP], this.reg.PC);
+                // this._push(this.reg.PC);
+      
+                // Push flags
+                this.reg[SP] = this.reg[SP] - 1;
+                this.ram.write(this.reg[SP], this.reg.FL);
+                // this._push(this.reg.FL);
+      
+                // Push registers R0-R6
+                for (let r = 0; r <= 6; r++) {
+                    this.reg[SP] = this.reg[SP] - 1;
+                    this.ram.write(this.reg[SP], this.reg[r]);
+                //   this._push(this.reg[r]);
+                }
+      
+                // Look up the vector (handler address) in the
+                // interrupt vector table
+                const vector = this.ram.read(0xf8 + i);
+      
+                this.reg.PC = vector; // Jump to it
+      
+                // Stop looking for more interrupts, since we do one
+                // at a time
+                break;
+              }
+            }
+          }
+
         // Load the instruction register (IR--can just be a local variable here)
         // from the memory address pointed to by the PC. (I.e. the PC holds the
         // index into memory of the instruction that's about to be executed
@@ -177,6 +269,33 @@ class CPU {
         const value = cpu.ram.read(cpu.reg[SP]);
         cpu.reg[SP]++;
         return value;
+    }
+
+    handle_ST(cpu, registerA, registerB) {
+        cpu.ram.write(cpu.reg[registerA], cpu.reg[registerB]);
+    }
+
+    handle_JMP(cpu, givenRegister) {
+        return cpu.reg[givenRegister];
+    }
+
+    handle_PRA(cpu, givenRegister) {
+        console.log(String.fromCharCode(cpu.reg[givenRegister]));
+    }
+
+    handle_IRET(cpu) {
+        for(let r = 6; r >= 0; r--) {
+            cpu.reg[r] = cpu.ram.read(cpu.reg[SP]);
+            cpu.reg[SP]++;
+        }
+        
+        cpu.reg.FL = cpu.ram.read(cpu.reg[SP]);
+        cpu.reg[SP]++;
+
+        // cpu.pcAdvance = true;
+        cpu.interruptsEnabled = true;
+
+        return cpu.ram.read(cpu.reg[SP]++);
     }
 
 }
